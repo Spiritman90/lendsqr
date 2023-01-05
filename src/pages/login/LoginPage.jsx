6fef167a8359aad33b97325b40a579006eaa4eed
spiritman90 <phemmyadey@gmail.com>
2023-01-05 13:21:41 +0100

    Completed mark up for Login page

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,10 +1,71 @@
-import React from "react";
 import "./LoginPage.scss";
+import LoginImage from "../../assets/img/pablo-sign-in 1.jpg";
+import Lendsqr from "../../assets/svgs/Lendsqr";
+import Logo from "../../assets/svgs/Logo";
+import ShowPasswordIcon from "../../assets/svgs/ShowPasswordIcon";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+// import { toast } from "react-toastify";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/dashboard");
+  };
   return (
-    <div>
-      <h1 className="login">Login Page</h1>
+    <div className="login">
+      <div className="login__logo-box">
+        <Logo />
+        <Lendsqr />
+      </div>
+
+      <div className="login__image-wrapper">
+        <img src={LoginImage} alt="login" className="login__photo" />
+        <div className="login__text-wrapper">
+          <h2 className="login__welcome">Welcome!</h2>
+          <p className="login__details">Enter details to login.</p>
+
+          <div className="login__info">
+            <form className="login__form" onSubmit={handleSubmit}>
+              <label className="login__label">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="login__input"
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                />
+              </label>
+              <label className="login__label">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Password"
+                  className="login__input"
+                  onChange={(e) => setPassword(e.target.value)}
+                  value={password}
+                />
+              </label>
+              <div className="login__show" onClick={handleShowPassword}>
+                <ShowPasswordIcon />
+              </div>
+              <p className="login__forgot-password">FORGOT PASSWORD?</p>
+              <button type="submit" className="login__button">
+                LOG IN
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routers.tsx
+++ src/routers.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { NonAuthRoutes, AuthRoutes } from "./urls";
+import { ToastContainer } from "react-toastify";
 import LoginPage from "./pages/login/LoginPage";
+import DashBoard from "./pages/dashboard/DashBoard";
 
 function Routers() {
   return (
@@ -8,8 +10,10 @@ function Routers() {
       <BrowserRouter>
         <Routes>
           <Route path={NonAuthRoutes.loginPage} element={<LoginPage />} />
+          <Route path={AuthRoutes.dashboard} element={<DashBoard />} />
         </Routes>
       </BrowserRouter>
+      <ToastContainer />
     </div>
   );
 }
