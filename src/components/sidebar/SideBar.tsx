2e4566a34c4edf3c5ff6df849d551552c81572ff
spiritman90 <phemmyadey@gmail.com>
2023-01-06 15:06:58 +0100

    Cretaed the Sidebar component

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,8 +1,134 @@
 import "./SideBar.scss";
+import UserFriends from "../../assets/svgs/UserFriends";
+import UsersIcon from "../../assets/svgs/UsersIcon";
+import SackIcon from "../../assets/svgs/SackIcon";
+import Piggybank from "../../assets/svgs/Piggybank";
+import HandshakeIcon from "../../assets/svgs/HandshakeIcon";
+import LoanRequestIcon from "../../assets/svgs/LoanRequestIcon";
+import WhitelistIcon from "../../assets/svgs/WhitelistIcon";
+import Karma from "../../assets/svgs/Karma";
+import Briefcase from "../../assets/svgs/Briefcase";
+import LoanProductsIcon from "../../assets/svgs/LoanProductsIcon";
+import SavingsAndProductsIcon from "../../assets/svgs/SavingsAndProductsIcon";
+import FeesAndCharges from "../../assets/svgs/FeesAndCharges";
+import TransactionIcon from "../../assets/svgs/TransactionIcon";
+import ServicesIcon from "../../assets/svgs/ServicesIcon";
+import ServiceAccountIcon from "../../assets/svgs/ServiceAccountIcon";
+import SettlementIcon from "../../assets/svgs/SettlementIcon";
+import ReportsIcon from "../../assets/svgs/ReportsIcon";
+import PreferencesIcon from "../../assets/svgs/PreferencesIcon";
+import FeesAndPricing from "../../assets/svgs/FeesAndPricing";
+import Audit from "../../assets/svgs/Audit";
+import DownArrow from "../../assets/svgs/DownArrow";
+import HomeIcon from "../../assets/svgs/HomeIcon";
+import Links from "../links/Links";
+
 const SideBar = () => {
+  const links1 = [
+    {
+      text: "Users",
+      icon: <UserFriends />,
+    },
+    {
+      text: "Guarantors",
+      icon: <UsersIcon />,
+    },
+    {
+      text: "Loans",
+      icon: <SackIcon />,
+    },
+    {
+      text: "Decision Models",
+      icon: <Piggybank />,
+    },
+    {
+      text: "Savings",
+      icon: <HandshakeIcon />,
+    },
+    {
+      text: "Loan Requests",
+      icon: <LoanRequestIcon />,
+    },
+    {
+      text: "Whitelist",
+      icon: <WhitelistIcon />,
+    },
+    {
+      text: "Karma",
+      icon: <Karma />,
+    },
+  ];
+  const links2 = [
+    {
+      text: "Organization",
+      icon: <Briefcase />,
+    },
+    {
+      text: "Loan Products",
+      icon: <LoanProductsIcon />,
+    },
+    {
+      text: "Savings Products",
+      icon: <SavingsAndProductsIcon />,
+    },
+    {
+      text: "Fees and Charges",
+      icon: <FeesAndCharges />,
+    },
+    {
+      text: "Transactions",
+      icon: <TransactionIcon />,
+    },
+    {
+      text: "Services",
+      icon: <ServicesIcon />,
+    },
+    {
+      text: "Service Account",
+      icon: <ServiceAccountIcon />,
+    },
+    {
+      text: "Settlements",
+      icon: <SettlementIcon />,
+    },
+    {
+      text: "Reports",
+      icon: <ReportsIcon />,
+    },
+  ];
+  const links3 = [
+    {
+      text: "Preferences",
+      icon: <PreferencesIcon />,
+    },
+    {
+      text: "Fees and Pricing",
+      icon: <FeesAndPricing />,
+    },
+    {
+      text: "Audit Logs",
+      icon: <Audit />,
+    },
+  ];
   return (
-    <div>
-      <h1>SideBar</h1>
+    <div className="sidebar">
+      <div className="sidebar__organization">
+        <Briefcase />
+        <p className="sidebar__heading">Switch Organization</p>
+        <DownArrow />
+      </div>
+      <div className="sidebar__dashboard">
+        <HomeIcon />
+        <p className="sidebar__heading2">Dashboard</p>
+      </div>
+      <p className="sidebar__brand">CUSTOMERS</p>
+      <div className="sidebar__links brand">
+        <Links links={links1} />
+        <p className="sidebar__brand">BUSINESSES</p>
+        <Links links={links2} />
+        <p className="sidebar__brand">SETTINGS</p>
+        <Links links={links3} />
+      </div>
     </div>
   );
 };
--- src/pages/dashboard/DashBoard.tsx
+++ src/pages/dashboard/DashBoard.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../../components/navbar/Navbar";
+import SideBar from "../../components/sidebar/SideBar";
 
 const dashboard = () => {
   return (
     <div>
       <Navbar />
+      <SideBar />
     </div>
   );
 };
